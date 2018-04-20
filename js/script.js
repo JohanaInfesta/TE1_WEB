8e43988c2d92ebecb0e9cf2d5427f3be3fe050c5
Johana Infesta <johana.infesta@gmail.com>
2018-04-20 16:15:41 -0300

    Animes.html arreglado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $.fn.imagesGrid.defaults.cells = 8;
 
 $('#imgs').imagesGrid({
-    images: ['../img/bleach.jpg','img',] //poner imagenes// hasta 20 img //despues arreglo el css //
+    images: ['../img/bleach.jpg','img',] //poner imagenes// hasta 20 img //despues arreglo el css // las img q sean tipo wallpapers
 
   });
