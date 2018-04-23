05a46a86e89b69d0fa3279b368713bfee74efac0
Johana Infesta <johana.infesta@gmail.com>
2018-04-23 19:34:42 -0300

    #2 Galeria de fotos completa y lista

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $.fn.imagesGrid.defaults.cells = 8;
 
 $('#imgs').imagesGrid({
-    images: ['img','img','img','img','img','img','img','img','img','img','img','img','img','img','img','img','img','img','img','img',] //poner imagenes// hasta 20 img //despues arreglo el css // las img q sean tipo wallpapers
+  images: [
+    '../img/wallpaper1.jpg','../img/wallpaper2.jpg',
+    '../img/wallpaper3.jpg','../img/wallpaper4.jpg',
+    '../img/wallpaper5.jpg','../img/wallpaper6.jpg',
+    '../img/death.jpg','../img/tokyo.jpg',
+    '../img/yugioh.jpg','../img/onepice.jpg',
+    '../img/rank7.jpg','../img/rank8.jpg',
+    '../img/rank9.jpg','../img/rank10.jpg',
+    '../img/masou.jpg','../img/dragonball.jpg',
+    '../img/katekyo.jpg','../img/highschool.jpg',
+    '../img/bleach.jpg','../img/theseven.jpg',] //poner imagenes// hasta 20 img //despues arreglo el css // las img q sean tipo wallpapers
 
   });
