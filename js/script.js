9acf21d23f307428138f6dcb350692f48004dedf
Johana Infesta <johana.infesta@gmail.com>
2018-04-30 15:00:34 -0300

    Arreglando js para el foro

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
 "use strict";
+let arreglo_nombre =[];
+let arreglo_comentario =[];
 
-let btn = document.getElementById('btn-enviar');
-let nombre = document.getElementById('form-nombre');
-let comentario = document.getElementById('form-comentario');
-
-btn.addEventListener("click",  function(info){
-  VerificarComentario(nombre, comentario);
-})
-
-function VerificarComentario(name, coment){
-
-   if ((name.value != "")&&(coment.value != "")) {
-
-    document.getElementById('foro-comentario').innerHTML = "<tr><td>" + name.value + "</td>" + "<td>" + coment.value + "</td></tr>";
-  }
-   else{
-    alert("Completar todos los campos");
-  }
-  // console.log(name);
+function Enviarr(){
+  let valor_nombre = document.getElementById('form-nombre').value;
+  let valor_comentario = document.getElementById('form-comentario').value;
+  arreglo_nombre.push(valor_nombre);
+  arreglo_comentario.push(valor_comentario);
+  console.log(arreglo_nombre);
+  Mostrar();
 }
+
+function Mostrar(){
+  document.getElementById('foro-comentario').innerHTML = "<tr><td>" + arreglo_nombre + "</td>" + "<td>" + arreglo_comentario + "</td></tr>";
+}
+
+// let btn = document.getElementById('btn-enviar');
+// let nombre = document.getElementById('form-nombre');
+// let comentario = document.getElementById('form-comentario');
+//
+// btn.addEventListener("click",  function(info){
+//   VerificarComentario(nombre, comentario);
+// })
+//
+// function VerificarComentario(name, coment){
+//
+//    if ((name.value != "")&&(coment.value != "")) {
+//
+//     document.getElementById('foro-comentario').innerHTML = "<tr><td>" + name.value + "</td>" + "<td>" + coment.value + "</td></tr>";
+//   }
+//    else{
+//     alert("Completar todos los campos");
+//   }
+//   // console.log(name);
+// }
 
 
 $.fn.imagesGrid.defaults.cells = 8;
